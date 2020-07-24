3a1495111c8538fe2ebe5d628216390abcce99cb
Xavi Fortes <53091080+XaviFortes@users.noreply.github.com>
2020-07-25 01:09:08 +0200

    Update rundos.sh

--- rundos.sh
+++ rundos.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
 clear
 wget -O /tmp/uwu.py 'https://raw.githubusercontent.com/XaviFortes/Python-UDP-Flood/master/udpflood.py'
 chmod +x /tmp/uwu.py
 cd /tmp
 clear
-python3 uwu.py
+while true; do
+    python uwu.py
+    case $? in
+    130) break ;;
+    esac
+    clear
+    figlet Hit another time Ctrl + C to exit -f slant
+    figlet You have 10 seconds -f smslant
+    sleep 10
+done
+clear
